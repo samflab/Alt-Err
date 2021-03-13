a23199f886dfc00f7fe1dbbd0754050514a0e17f
samflab <kasturimeh@gmail.com>
2021-03-13 08:22:36 +0530

    hj

---
+++ Assets/BackButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    public void Back(){
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+   }
+}
---
+++ Assets/ExitGame.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    public void Exit(){
+       Application.Quit();
+   }
+}
